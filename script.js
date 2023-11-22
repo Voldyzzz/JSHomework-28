09553d5090552b55455905a2285b9d5b7782559f
Voldyzzz <volodymyr.liuliev2000@gmail.com>
2023-11-22 21:25:42 +0200

    Update script.js
    -fix function

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-console.log(numToDegree(2, 3));
+console.log(numToDegree(5, 3));
 
 function numToDegree(num, degree) {
-  degree--;
-  num = num * num;
-
-  if (degree > 1) {
-    numToDegree(num, degree);
+  if (degree === 0) {
+    return 1;
+  } else {
+    return num * numToDegree(num, --degree);
   }
-
-  return num;
 }
